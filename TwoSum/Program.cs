ae5eac625eec5b619a25314cbc6df1190230e3fe
Billy <beyond_bill_josh@hotmail.com>
2022-12-12 14:12:28 -0500

    Uploading Second Proyect

---
+++ TwoSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+/*
+ * @author Billy Bedon Pacheco
+ */
+
+namespace TwoSum
+{
+    public class Program : TwoSum
+    {
+        public static void Main()
+        {
+            //{2,7,11,15} Target: 9     Expectation: 0,1
+            //{2,7,11,15} Target: 18     Expectation: 1,2
+            //{2,7,11,15} Target: 22     Expectation: 2,3
+            //{3,2,4}     Target: 6     Expectation: 1,2
+            //{3,3}       Target: 6     Expectation: 0,1
+            int[] numberArray = new int[] { 2, 7, 11, 15 };
+            int target = 18;
+            int[] result = SumTwoNumTarget(numberArray, target); //return the indices of the 2 numbers such that they add up to the target value.
+            Console.WriteLine("The index of each number that they add up to the target value are:");
+            Console.WriteLine($"[{result[0]}, {result[1]}]");
+        }
+    }
+}
---
+++ TwoSum/TwoSum.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+/*
+ * @author Billy Bedon Pacheco
+ */
+
+namespace TwoSum
+{
+    public class TwoSum
+    {
+        public static int[] SumTwoNumTarget(int[] nums, int target)
+        {
+            Dictionary<int, int> numDictionary = new Dictionary<int, int>(); //Using Dictionary every value gets a key so it wont read an item twice.
+
+            int[] indicesResult = new int[2]; //We store result here
+
+            for (int index = 0; index < nums.Length; index++)
+            {
+                int indexValue = target - nums[index];
+
+                if (numDictionary.ContainsKey(indexValue) && index != numDictionary[indexValue])//if keyValue is already in Dictionary and index is distinct than the indexValue in Dictionary then return saved indices.
+                {
+                    indicesResult[0] = numDictionary[indexValue]; //indexValue would return numDictionary selected item index.
+                    indicesResult[1] = index;   //If Index is different than the value of the selected item in numDirectory, it saves the second index value.
+                    return indicesResult;
+                }
+                numDictionary[nums[index]] = index;
+            }
+            return indicesResult;
+        }
+    }
+}
